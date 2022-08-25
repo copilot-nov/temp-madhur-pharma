fadac2c0159d65edcf2d1d8645f35a02fac3a88b
prakash-simhandri <prakashsimhadri.com@gmail.com>
2022-08-26 00:36:40 +0530

    sweetalert added when cancel modal

--- src/components/productionBatchList/AddProductionBatch.js
+++ src/components/productionBatchList/AddProductionBatch.js
@@ -4,14 +4,16 @@ import { connect } from 'react-redux';
 import AutoSearch from '../autoComplete';
 import { Dialog, Transition } from '@headlessui/react';
 import { ADD_PRODUCTION_BATCH } from '../../redux/actions/production';
+import Swal from "sweetalert2";  
+
 
 const defaultState = {
 
 }
 const ProductionBatch = (props) => {
-    const { open, closeModal, setOpen, materialList, masterDataList, productList, orderList, ADD_PRODUCTION_BATCH,setHandleResponse,handleResponse } = props;
+    const { open, closeModal, setOpen, materialList, masterDataList, productList, orderList, ADD_PRODUCTION_BATCH, setHandleResponse, handleResponse } = props;
     let UOMList = masterDataList?.filter((item) => item?.type_id === 32);
-    let filterMaterialList = materialList?.filter((item)=> item.sku === true);
+    let filterMaterialList = materialList?.filter((item) => item.sku === true);
     const [payload, setPayload] = useState(defaultState)
     const [SKU, setSKU] = useState(filterMaterialList)
     const [UOM, setUOM] = useState(UOMList)
@@ -24,7 +26,20 @@ const ProductionBatch = (props) => {
         let { name, value, type } = e.target
         setPayload({ ...payload, [name]: type === 'number' ? Number(value) : value })
     }
-    // console.log(SKU)
+
+    const closeModalBox = (e) => {
+        Swal.fire({
+            text: "Are you sure you want to exit ?",
+            icon: 'warning',
+            showCancelButton: true,
+            cancelButtonColor: '#d33',
+        })
+            .then((res) => {
+                if(res.value){
+                    closeModal()
+                }
+            });
+    }
 
     const handleSubmit = async (e) => {
         e.preventDefault()
@@ -249,7 +264,7 @@ const ProductionBatch = (props) => {
                                                 </div>
                                                 <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
                                                     <button
-                                                        onClick={closeModal}
+                                                        onClick={() => { closeModalBox() }}
                                                         className="mr-2 inline-flex justify-center py-2 px-8 border border-green-900 shadow-sm text-sm font-medium text-green-900 bg-white hover:bg-green-100 hover:text-green-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
                                                     >
                                                         Cancel
